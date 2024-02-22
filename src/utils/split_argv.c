909126942d22386e77bf1c90758a49e0e85258f7
octoross-42 <codingoceane@gmail.com>
2024-02-22 12:12:08 +0100

    split

--- src/utils/split_argv.c
+++ src/utils/split_argv.c
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 03:35:04 by octoross          #+#    #+#             */
-/*   Updated: 2024/02/21 19:49:43 by octoross         ###   ########.fr       */
+/*   Updated: 2024/02/22 11:37:32 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,41 +86,67 @@ size_t	ft_nbr_words(char const *s)
 	int		nest_two;
 
 	nbr_words = 0;
+	nest_one = 0;
+	nest_two = 0;
 	i = 0;
 	while (s[i] && s[i] == ' ')
 		i ++;
 	while (s[i])
 	{
 		nbr_words ++;
-		while (s[i] && s[i] != ' ')
+		while (s[i] && s[i] != ' ' && !nest_one && !nest_two)
+		{
+			if (s[i] == '\'' && !nest_one)
+				nest_one ++;
+			else if (s[i] == '\'')
+				nest_one --;
+			else if (s[i] == '"' && !nest_two)
+				nest_two ++;
+			else if (s[i] == '"')
+				nest_two --;
 			i ++;
+		}
 		while (s[i] && s[i] == ' ')
 			i ++;
 	}
 	return (nbr_words);
 }
 
-char	*ft_next_word(const char *s, size_t *i, char c)
+char	*ft_next_word(const char *s, size_t *i)
 {
 	char	*word;
 	int		len;
+	int		nest_one;
+	int		nest_two;
 
 	len = 0;
-	while (s[*i + len] && s[*i + len] != c)
+	while (s[*i + len] && s[*i + len] != ' ')
 		len ++;
 	word = (char *)malloc(sizeof(char) * (len + 1));
 	if (!word)
 		return (0);
 	word[len] = '\0';
 	len = 0;
-	while (s[*i] && s[*i] != c)
+	while (s[i] && s[i] != ' ' && !nest_one && !nest_two)
+	{
+		if (s[i] == '\'' && !nest_one)
+			nest_one ++;
+		else if (s[i] == '\'')
+			nest_one --;
+		else if (s[i] == '"' && !nest_two)
+			nest_two ++;
+		else if (s[i] == '"')
+			nest_two --;
+		i ++;
+	}
+	while (s[*i] && s[*i] != ' ')
 		word[len++] = s[(*i)++];
-	while (s[*i] && s[*i] == c)
+	while (s[*i] && s[*i] == ' ')
 		(*i)++;
 	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split_argv(char const *s)
 {
 	size_t	nbr_words;
 	size_t	i;
@@ -128,21 +154,41 @@ char	**ft_split(char const *s, char c)
 
 	if (!s)
 		return (0);
-	nbr_words = ft_nbr_words(s, c);
+	nbr_words = ft_nbr_words(s);
 	splited = (char **)malloc(sizeof(char *) * (nbr_words + 1));
 	if (!splited)
 		return (0);
 	splited[nbr_words] = 0;
 	nbr_words = 0;
 	i = 0;
-	while (s[i] && s[i] == c)
+	while (s[i] && s[i] == ' ')
 		i ++;
 	while (s[i])
 	{
-		splited[nbr_words] = ft_next_word(s, &i, c);
+		splited[nbr_words] = ft_next_word(s, &i);
 		if (!splited[nbr_words])
 			return (ft_free_until((void **)splited, nbr_words), NULL);
 		nbr_words ++;
 	}
 	return (splited);
+}
+
+int	main(int argc, char **argv)
+{
+	char 	**splited;
+	int		i;
+
+	printf("argv : %s\n", argv[1]);
+	splited = ft_split_argv(argv[1]);
+	if (!splited)
+		return (printf("(null)"), 0);
+	i = 0;
+	while (splited[i])
+	{
+		printf("i : '%s'\n", splited[i]);
+		free(splited[i]);
+		i ++;
+	}
+	free(splited);
+	return (0);
 }
